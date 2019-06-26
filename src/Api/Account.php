5c597b65c5aa20a9f9618696bba8b40eccdb50a2
lin <465382251@qq.com>
2019-06-26 15:34:13 +0800

    add note

--- src/Api/Account.php
+++ src/Api/Account.php
@@ -10,14 +10,16 @@ use Lin\Exchange\Interfaces\AccountInterface;
 class Account extends Base implements AccountInterface
 {
     /**
-     *
-     * */
-    function position(array $data){
-        
-    }
-    
-    /**
-     *
+     * 现货返回查询余额，期货返回仓位数量
+     * binance $exchanges->account()->get();返回所有货币对余额
+     * bitmex $exchanges->account()->get();返回所有持仓
+     * 
+     * huobi spot $exchanges->account()->get(['_symbol'=>'btcusdt']);返回指定币对余额
+     * 
+     * okex spot $exchanges->account()->get(['_symbol'=>'BTC']);返回指定币对余额
+     * okex future $exchanges->account()->get(['_symbol'=>'BTC-USD-190628',]);返回指定币对余额
+     * 
+     * @return array 返回原始数据
      * */
     function get(array $data=[]){
         try {
@@ -28,4 +30,14 @@ class Account extends Base implements AccountInterface
             return $this->error($e->getMessage());
         }
     }
+    /**
+     * 暂时无用，保留坑位
+     * @see \Lin\Exchange\Interfaces\AccountInterface::position()
+     */
+    public function position(array $data)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
