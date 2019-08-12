2d3d192335a30b7716aa938c9998e96f5c190a55
lin <465382251@qq.com>
2019-08-12 16:58:34 +0800

    Okex precision loss

--- src/Map/ResponseTraderMap.php
+++ src/Map/ResponseTraderMap.php
@@ -349,10 +349,12 @@ class ResponseTraderMap extends Base implements TraderInterface
                 //判断是期货还是现货
                 switch ($this->checkType($data['result']['instrument_id'])){
                     case 'spot':{
+                        //okex 小币种 精度又丢失的情况  不如dash-usdt  filled_notional:只精度到0.1位  所以采用倒推的方式  filled_notional=price_avg*filled_size 
+                        $map['_filed_amount']=bcmul(strval($data['result']['price_avg']),strval($data['result']['filled_size']),16);
+                        
                         $map['_filled_qty']=$data['result']['filled_size'];
-                        $data['result']['filled_size']==0?$map['_price_avg']=0:$map['_price_avg']=bcdiv(strval($data['result']['filled_notional']),strval($data['result']['filled_size']),16);
+                        $data['result']['filled_size']==0 ? $map['_price_avg']=0 : $map['_price_avg']=bcdiv(strval($map['_filed_amount']),strval($data['result']['filled_size']),16);
                         $map['_status']=$this->okex_status['spot'][$data['result']['state']];
-                        $map['_filed_amount']=$data['result']['filled_notional'];
                         break;
                     }
                     case 'future':{
